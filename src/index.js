ae2e677a6ccf251dcb795e3a002752f51fca8f86
stigerman <inxcess1@gmail.com>
2018-07-09 14:02:59 -0400

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Carousel from './Carousel';
-
-ReactDOM.render( <
-    Carousel / > , document.getElementById('root'));
